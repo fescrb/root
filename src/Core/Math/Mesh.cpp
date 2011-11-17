79ec534616f7b15ede18658e6d1ff4783e5f0f14
Fernando Escribano Macias <f.escrb@gmail.com>
2011-11-17 20:43:50 +0000

    Mesh class added.

---
+++ src/Core/Math/Mesh.cpp
@@ -0,0 +1,19 @@
+/*
+ * Mesh.cpp
+ *
+ *  Created on: Nov 17, 2011
+ *      Author: fernando
+ */
+
+#include "Mesh.h"
+
+using namespace root;
+
+mesh::mesh() {
+	// TODO Auto-generated constructor stub
+
+}
+
+mesh::~mesh() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/Core/Math/Mesh.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2011  Fernando Escribano Macias
+ *
+ * This file is part of the Root Engine.
+ *
+ * The Root Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Root Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Root Engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ROOT_MESH_H_
+#define _ROOT_MESH_H_
+
+namespace root {
+
+	struct mesh {
+	public:
+		mesh();
+		virtual ~mesh();
+	};
+
+}
+
+#endif /* _ROOT_MESH_H_ */
