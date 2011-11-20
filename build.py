287affbe4fd0edef82aad7713e1e46c92ba0f301
Fernando Escribano <f.escrb@gmail.com>
2011-11-20 17:08:23 +0000

    Able to show verbose output and add extra flags.

--- build.py
+++ build.py
@@ -1,12 +1,34 @@
 import os
 import sys
 
+# Declare variables
+extra_flags= ""
+verbose = ""
+
+
+if len(sys.argv) > 1 :
+	argc = 1
+	while argc < len(sys.argv) :
+		command = sys.argv[argc]
+		if command == "extraflags":
+			argc+=1
+			extra_flags = sys.argv[argc]
+		argc+=1
+		if command == "verbose":
+			verbose = "VERBOSE=1"
+	
+
 if not os.path.exists('obj'):
     os.mkdir('obj')
     
 os.chdir('obj')
 
-if not os.system('cmake .. -DROOT_USE_SIMD=1'):
-    os.system('make')
+cmake_command = 'cmake .. -DROOT_USE_SIMD=1 -DEXTRA_FLAGS="' + extra_flags + '" '
+make_command = 'make ' + verbose
+
+print(cmake_command)
+
+if not os.system(cmake_command):
+    os.system(make_command)
 else:
     sys.exit(1)
